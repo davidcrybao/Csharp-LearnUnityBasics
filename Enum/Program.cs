fde1a77f231f623ff87119e5addea0bec6f0491f
VIKTOR RENEV <easykillme@outlook.com>
2024-06-26 22:05:41 +0900

    错误的代码,一直在想,错误在哪里 你的代码的主要问题在于，它只是将相邻元素的大小进行了比较和交换，而没有实现对整个数组的排序。此外，每次循环都新建 tempArray 也会导致上一次循环的排序结果被覆盖。
    让我们用一个具体的例子来说明：
    
    假设 array = {3, 1, 4, 2}， 你的代码执行过程如下：
    
    第一次循环 (i = 1):
    
    比较 array[0] 和 array[1]， 即 3 和 1。
    因为 3 > 1， 执行 else 语句块：
    max = array[0] = 3
    tempArray[1] = max = 3
    tempArray[0] = array[1] = 1
    此时 tempArray = {1, 3, 0, 0}
    第二次循环 (i = 2):
    
    比较 array[1] 和 array[2]， 即 1 和 4。
    因为 1 < 4， 执行 if 语句块：
    max = array[2] = 4
    tempArray[1] = array[1] = 1
    tempArray[2] = max = 4
    此时 tempArray = {1, 1, 4, 0}， 注意这里 tempArray[1] 被错误地覆盖了。
    第三次循环 (i = 3):
    
    比较 array[2] 和 array[3]， 即 4 和 2。
    因为 4 > 2， 执行 else 语句块：
    max = array[2] = 4
    tempArray[3] = max = 4
    tempArray[2] = array[3] = 2
    此时 tempArray = {1, 1, 2, 4}
    最终输出的 tempArray 并不是排序后的结果。
    
    为了实现对整个数组的排序， 你需要学习和使用冒泡排序、插入排序等经典排序算法。 这些算法能够正确地比较和交换数组元素， 最终将数组按照从小到大的顺序排列。

---
+++ Enum/Program.cs
@@ -0,0 +1,180 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Enum2
+{
+    internal class Program
+    {
+        enum E_Type
+        {
+            Monster,
+            Player,
+            Loser,
+        }
+        enum E_QQState
+        {
+            online,
+            busy,
+            offline,
+            callme,
+        }
+        enum E_Strarbucks
+        {
+            SuperBig,
+            Big,
+            Middle,
+            Small,
+
+        }
+        enum E_Gender
+        {
+            male,
+            female,
+        }
+        enum E_Profession
+        {
+            warrior,
+            hunter,
+            mage,
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            E_Type playerType = E_Type.Player;
+            E_QQState e_QQState;
+            playerType = (E_Type)Enum.Parse(typeof(E_Type), "Loser");
+
+            Console.WriteLine("请选择QQ的状态,0-在线,1-忙绿,2-不在线,3-联系我");
+            int inputState2 = int.Parse(Console.ReadLine());
+            e_QQState = (E_QQState)inputState2;
+            Console.Write("目前是{0}", e_QQState);
+
+
+            /*           #region 题目2
+
+                       *//*while (true)
+                       {
+                           Console.WriteLine("请输入您想要的杯型(0-SuperBig, 1-Big, 2-Middle, 3-Small):");
+                           bool hasChoose = false;
+                           string inputState = Console.ReadLine();
+                           if (int.TryParse(inputState, out int index))
+                           {
+                               if (Enum.IsDefined(typeof(E_Strarbucks), index))
+                               {
+                                   E_Strarbucks size = (E_Strarbucks)Enum.ToObject(typeof(E_Strarbucks), index);
+                                   switch (size)
+                                   {
+                                       case E_Strarbucks.SuperBig:
+                                           Console.WriteLine("您选择了超大杯,花费了43元");
+                                           break;
+                                       case E_Strarbucks.Big:
+                                           Console.WriteLine("您选择了大杯,花费了40元");
+                                           break;
+                                       case E_Strarbucks.Middle:
+                                           Console.WriteLine("您选择了中杯,花费了35元");
+                                           break;
+                                       case E_Strarbucks.Small:
+                                           Console.WriteLine("您选择了小杯,这是免费送的,地上回收的");
+                                           break;
+                                   }
+                                   hasChoose = true;
+                               }
+                               else
+                               {
+                                   Console.WriteLine("输入的不对,请重新输入");
+                               }
+                           }
+
+
+                           if (hasChoose)
+                           {
+                               break;
+                           }
+
+                       }*//*
+
+                       Console.WriteLine("请输入数字 (0: SuperBig, 1: Big, 2: Middle, 3: Small):");
+                       string input = Console.ReadLine();
+
+                       if (int.TryParse(input, out int index))
+                       {
+                           if (Enum.IsDefined(typeof(E_Strarbucks), index))
+                           {
+                               E_Strarbucks size = (E_Strarbucks)index;
+                               Console.WriteLine($"你选择的尺寸是: {size}");
+                           }
+                           else
+                           {
+                               Console.WriteLine("输入的数字不在有效范围内，请输入0到3之间的数字。");
+                           }
+                       }
+                       else
+                       {
+                           Console.WriteLine("输入无效，请输入一个数字。");
+                       }
+                       #endregion
+           */
+
+            #region 题目3
+
+            int defence = 0;
+            int attack = 0;
+            E_Gender e_Gender;
+            E_Profession e_Profession;
+            Console.Write("当前攻击的{0},防御为{1}", attack, defence);
+            Console.WriteLine("请选择你的性别,0-男,1女");
+            string inputState = Console.ReadLine();
+            if (int.TryParse(inputState, out int secondIndex))
+            {
+                if (secondIndex == 0)
+                {
+                    e_Gender = E_Gender.male;
+                    attack += 50;
+                    defence += 100;
+                    Console.WriteLine("你选择了男性,现在攻击力为{0},防御力为{1}", attack, defence);
+                }
+                else if (secondIndex == 1)
+                {
+                    e_Gender = E_Gender.female;
+                    attack += 150;
+                    defence += 20;
+                    Console.WriteLine("你选择了女性,现在攻击力为{0},防御力为{1}", attack, defence);
+                }
+
+            }
+            Dictionary<E_Profession, string> professionSkill = new Dictionary<E_Profession, string>
+            {
+                { E_Profession.warrior,"冲刺" },
+                 { E_Profession.hunter,"假死" },
+                  { E_Profession.mage,"奥术冲击" },
+            };
+            Console.WriteLine("请选择你的职业,0-战士,1-猎人,2-法师");
+            inputState = Console.ReadLine();
+            if (int.TryParse(inputState, out int Index))
+            {
+                switch (Index)
+                {
+                    case 0:
+                        e_Profession = E_Profession.warrior;
+                        attack += 20;
+                        defence += 100;
+                        Console.WriteLine("你选择了{0},当前的攻击力为{1},防御力为{2},你现在有技能{3}", e_Profession, attack, defence, professionSkill[e_Profession]);
+                        break;
+                    case 1:
+                        e_Profession = E_Profession.hunter;
+                        attack += 120;
+                        defence += 30;
+                        Console.WriteLine("你选择了{0},当前的攻击力为{1},防御力为{2},你现在有技能{3}", e_Profession, attack, defence, professionSkill[e_Profession]);
+                        break;
+                    case 2:
+                        e_Profession = E_Profession.mage;
+                        attack += 200;
+                        defence += 10;
+                        Console.WriteLine("你选择了{0},当前的攻击力为{1},防御力为{2},你现在有技能{3}", e_Profession, attack, defence, professionSkill[e_Profession]);
+                        break;
+                }
+
+            }
+            #endregion
+        }
+    }
+}
