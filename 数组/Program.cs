f809acfb33eb5eae01e1226134e675a50fe3e1f1
VIKTOR RENEV <easykillme@outlook.com>
2024-06-26 22:05:41 +0900

    错误的代码,一直在想,错误在哪里 你的代码的主要问题在于，它只是将相邻元素的大小进行了比较和交换，而没有实现对整个数组的排序。此外，每次循环都新建 tempArray 也会导致上一次循环的排序结果被覆盖。
    让我们用一个具体的例子来说明：
    
    假设 array = {3, 1, 4, 2}， 你的代码执行过程如下：
    
    第一次循环 (i = 1):
    
    比较 array[0] 和 array[1]， 即 3 和 1。
    因为 3 > 1， 执行 else 语句块：
    max = array[0] = 3
    tempArray[1] = max = 3
    tempArray[0] = array[1] = 1
    此时 tempArray = {1, 3, 0, 0}
    第二次循环 (i = 2):
    
    比较 array[1] 和 array[2]， 即 1 和 4。
    因为 1 < 4， 执行 if 语句块：
    max = array[2] = 4
    tempArray[1] = array[1] = 1
    tempArray[2] = max = 4
    此时 tempArray = {1, 1, 4, 0}， 注意这里 tempArray[1] 被错误地覆盖了。
    第三次循环 (i = 3):
    
    比较 array[2] 和 array[3]， 即 4 和 2。
    因为 4 > 2， 执行 else 语句块：
    max = array[2] = 4
    tempArray[3] = max = 4
    tempArray[2] = array[3] = 2
    此时 tempArray = {1, 1, 2, 4}
    最终输出的 tempArray 并不是排序后的结果。
    
    为了实现对整个数组的排序， 你需要学习和使用冒泡排序、插入排序等经典排序算法。 这些算法能够正确地比较和交换数组元素， 最终将数组按照从小到大的顺序排列。

---
+++ 数组/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace 数组
+{
+    internal class Program
+    {
+        /*        基础练习题目
+声明一个包含10个整数的数组，并为其赋值，然后输出所有元素。
+编写一个程序，计算并输出数组元素的平均值。
+进阶练习题目
+声明一个包含10个整数的数组，编写一个程序，找出并输出数组中所有偶数。
+编写一个程序，对一个包含10个整数的数组进行排序（从小到大）。*/
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int[] index = { 1, 35, 88, 4, 5, 6, 12, 8, 9, 10 };
+            for (int i = 0; i < index.Length; i++)
+            {
+                Console.WriteLine(index[i]);
+            }
+            CalculateAverageArray(index);
+
+            FindAllEvenInArray(index);
+            ResortArrayLowerToHigher(index);
+        }
+        private static void CalculateAverageArray(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            Console.WriteLine("数组总共有{0}个数,平均数是{1}", array.Length, (float)sum / array.Length);
+
+        }
+
+        private static void FindAllEvenInArray(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    count++;
+                    Console.WriteLine("偶数:" + array[i]);
+                }
+
+            }
+
+            Console.WriteLine("总共偶数数量是{0}", count);
+
+        }
+
+        private static void ResortArrayLowerToHigher(int[] array)
+        {
+            int[] tempArray = new int[array.Length];
+            int max;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] < array[i])
+                {
+                    max = array[i];
+                    tempArray[i - 1] = array[i - 1];
+                    tempArray[i] = max;
+                }
+                else
+                {
+                    max = array[i - 1];
+                    tempArray[i] = max;
+                    tempArray[i - 1] = array[i];
+                }
+
+
+            }
+            Console.WriteLine("重新排序之后");
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                Console.Write("  " + tempArray[i]);
+            }
+
+        }
+
+
+    }
+}
