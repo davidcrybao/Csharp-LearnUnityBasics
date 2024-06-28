0cd35f50d07da495df104c7d3467a246afdf1056
VIKTOR RENEV <easykillme@outlook.com>
2024-06-28 11:27:26 +0900

    增加了一系列基础的学习

---
+++ ref&out/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace ref_out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请一次输入用户名密码");
+            string password = "";
+            string username = "";
+
+            username = Console.ReadLine();
+            password = Console.ReadLine();
+
+            bool result = CheckUserPass(ref username, ref password);
+
+            Console.WriteLine("你的输入是{0},当前用户名是{1},密码是{2}", result ? "对" : "错", username, password);
+
+        }
+
+        static bool CheckUserPass(ref string user, ref string pwd)
+        {
+            Console.WriteLine("正在核对信息");
+            if (user == "888")
+            {
+                if (pwd == "666")
+                {
+                    return true;
+                }
+                else
+                {
+                    pwd = "密码错误";
+                }
+
+                return false;
+            }
+            else
+            {
+                user = "用户名错误";
+                return false;
+            }
+        }
+        static void ExchangeValue(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void FindMaxAndMin(int[] array, out int max, out int min)
+        {
+            max = array[0];
+            min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+
+        }
+
+        static void ResortArray(ref int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+
+            }
+
+        }
+        static void FindArray(ref string[] array, out int length, out string text)
+        {
+            length = array[0].Length;
+            int number = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (length < array[i].Length)
+                {
+                    length = array[i].Length;
+                    number = i;
+                }
+
+            }
+
+            text = array[number];
+
+        }
+
+
+    }
+}
---
+++ string/Program.cs
@@ -0,0 +1,60 @@
+﻿
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array1 = { 1, 2, 33, 45, 564, 545, -1 };
+        int c = 0;
+        CompareNumbers(ref c, 5, 6);
+        Console.WriteLine(c);
+
+
+        Console.WriteLine(CheckLeapYear(2006));
+    }
+
+    static int CompareNumbers(ref int c, int a, int b)
+    {
+        c = 5;
+        int bigger = a > b ? a : b;
+        return bigger;
+    }
+
+
+    static int[] CircleAndPerimeter(int a)
+    {
+        int circle = (int)(a * a * Math.PI);
+        int Perimeter = (int)(2 * a * Math.PI);
+        return new int[] { circle, Perimeter };
+    }
+
+    static void CompareNumbers(int[] a)
+    {
+        int sum = 0;
+        int max = a[0];
+        int min = a[0];
+        for (int i = 0; i < a.Length; i++)
+        {
+            sum += a[i];
+            if (max < a[i])
+            {
+                max = a[i];
+            }
+
+            if (min > a[i])
+            {
+                min = a[i];
+            }
+
+        }
+        Console.WriteLine("总和是{0},最大值是{1},最小值是{2},平均值是{3}", sum, max, min, sum / a.Length);
+    }
+    static bool CheckLeapYear(int year)
+    {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
