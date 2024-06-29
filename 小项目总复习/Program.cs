2bfa24172217dee4bab21846cc8bf47fecf661f5
VIKTOR RENEV <easykillme@outlook.com>
2024-06-29 23:50:22 +0900

    是不是可以增加一个bool 来判断,如果我们划过了这个,就去画另外一边的墙,这样子墙就是一条直线

--- 小项目总复习/Program.cs
+++ 小项目总复习/Program.cs
@@ -2,6 +2,17 @@
 {
     internal class Program
     {
+        public struct Vector2
+        {
+            public int x;
+            public int y;
+
+            public Vector2(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
         static void Main(string[] args)
         {
             Random r = new Random();
@@ -59,9 +70,7 @@
 
                     case 1:
                         Console.Clear();
-
-                        int startXPosition = 0;
-                        int startYPosition = 0;
+                        Vector2 startPosition = new Vector2(0, 0);
                         int endXPosition = 0;
                         int endYPosition = 0;
                         while (true)
@@ -93,11 +102,33 @@
                             //我们是在最上面的生成地图的 所以坐标(2,1)开始 到坐标(46,28) 复杂的逻辑 生成其他的时候记录count+二元数组坐标(或者xy两个数组);
                             //(2,29-46.34) 是我们说明的文档生成 最简单的一部分
                             //(2.35-46.38)是我们按键互动之后生成,这里面的逻辑比较复杂
-                            startXPosition = r.Next(2, 46);
-                            startYPosition = r.Next(2, 5);
+                            startPosition.x = r.Next(2, 15);
+                            startPosition.y = r.Next(2, 5);
                             endXPosition = r.Next(2, 46);
                             endYPosition = r.Next(25, 28);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.SetCursorPosition(startPosition.x, startPosition.y);
+                            for (int i = 0; i < 12; i++)
+                            {
 
+                                for (int j = 0; j < 24; j += 2)
+                                {
+                                    if (i % 2 == 0)
+                                    {
+                                        Console.SetCursorPosition(startPosition.x + j, startPosition.y + i);
+                                        Console.Write("□");
+                                    }
+                                    else if (j == 22)
+                                    {
+                                        Console.SetCursorPosition(startPosition.x + j, startPosition.y + i);
+                                        Console.Write("□");
+                                    }
+
+                                }
+
+
+
+                            }
                             #endregion
 
 
