d2b04ec91cc2c0dc641c2e0a7b77ddde65b76570
VIKTOR RENEV <easykillme@outlook.com>
2024-07-20 07:23:00 +0900

    学习呀

--- C#进阶学习/9.0顺序存储和链式存储/Program.cs
+++ C#进阶学习/9.0顺序存储和链式存储/Program.cs
@@ -54,20 +54,42 @@ namespace _9._0顺序存储和链式存储
                 return;
             }
       
-            LinkedNode<T> temp = last;
-            while (temp.nextNode != null)
+            LinkedNode<T> current = head;
+            LinkedNode<T> previous = null;
+
+            while (current!=null&&!current.value.Equals(node))
             {
-                if (temp.value.Equals(node))
-                {
-                    
-                    temp.nextNode = temp.nextNode.nextNode;
-                }
-                else
-                {
-                    temp = temp.nextNode;
-                }
-             
+                previous = current;
+                current = current.nextNode;
+
             }
+            if (current!=null)
+            {
+                //如果当前已经是最后一个节点,就是最后一个节点的值等于我们的node
+                if (current==last)
+                {
+                    //改变链表里面的最后一个
+                    last = previous;
+                }
+                //正常逻辑 都要修改 前一个节点等于我们的现在节点的下一个节点 切割
+                previous.nextNode = current.nextNode;
+            }
+            else
+            {
+                Console.WriteLine("未找到要删除的节点。");
+            }
+
+        }
+
+        public void PrintList()
+        {
+            LinkedNode<T> current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.value);
+                current = current.nextNode;
+            }
+
         }
     }
 
@@ -79,7 +101,24 @@ namespace _9._0顺序存储和链式存储
             LinkedNode<int> t2 = new LinkedNode<int>(2);
             t1.nextNode = t2;
             Console.WriteLine(t1.nextNode.value);
-            
+
+            LinkedList<int> list = new LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            Console.WriteLine("链表中的元素：");
+            list.PrintList();
+
+            list.Remove(3);
+            Console.WriteLine("删除值为 3 的节点后链表中的元素：");
+            list.PrintList();
+
+            list.Remove(1);
+            Console.WriteLine("删除值为 1 的节点后链表中的元素：");
+            list.PrintList();
+
         }
     }
 }
