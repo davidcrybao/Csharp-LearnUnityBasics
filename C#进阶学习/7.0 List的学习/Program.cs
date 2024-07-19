411e34867d639f659a8460c5ab5605bb61f885ea
VIKTOR RENEV <easykillme@outlook.com>
2024-07-20 07:14:35 +0900

    链式需要加强学习

---
+++ C#进阶学习/7.0 List的学习/Program.cs
@@ -0,0 +1,160 @@
+﻿
+using System.Threading.Tasks;
+
+namespace _7._0_List的学习
+{
+
+    public class TaskManager
+
+
+    {
+        public List<string> tasks;
+        
+        public TaskManager()
+        {
+            tasks = new List<string>();
+        }
+        public void Add(string task)
+        {
+            tasks.Add(task);
+            Console.WriteLine($"任务 '{task}' 已添加。");
+        }
+
+        public void Remove(string task)
+        {
+            if (tasks.Count<=0)
+            {
+                Console.WriteLine("没有任务可以移除");
+                return;
+            }
+            else
+            {
+                tasks.Remove(task);
+            }
+
+        }
+        public bool Find(string task)
+        {
+            if (tasks.Count <= 0)
+            {
+                Console.WriteLine("a你都没任务啊查找什么?");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("查询中");
+                return tasks.Contains(task);
+            }
+        }
+
+        public void Display()
+        {
+            if (tasks.Count <= 0)
+            {
+                Console.WriteLine("你都没任务啊!!!!!!!");
+                return ;
+            }
+            else
+            {
+                Console.WriteLine("任务有:");
+                for (int i = 0; i < tasks.Count; i++)
+                {
+                    Console.WriteLine(tasks[i]);
+                }
+            }
+
+        }
+
+    }
+
+    public abstract class Monster
+    {
+        public static List<Monster> monsters1;
+        static Monster()
+        {
+            monsters1 = new List<Monster>();
+        }
+        public Monster()
+        {
+            monsters1.Add(this);
+        }
+        public abstract void Attack();
+ 
+    }
+
+  
+    public class Boss : Monster
+    {
+        public override void Attack()
+        {
+            Console.WriteLine("Boss Attack");
+        }
+    }
+    public class Goblin : Monster
+    {
+        public override void Attack()
+        {
+            Console.WriteLine("Goblin Attack");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            ints.RemoveAt(5);
+            for (int i = 0; i < ints.Count; i++)
+            {
+                Console.WriteLine(ints[i]);
+            }
+
+            List<Monster> monsters = new List<Monster>();
+            Goblin goblin = new Goblin();
+            Boss boss = new Boss();
+            Goblin goblin2 = new Goblin();
+            Monster.monsters1[0].Attack();
+
+            Monster.monsters1[1].Attack();
+            Console.WriteLine(Monster.monsters1.Count);
+
+            TaskManager taskManager = new TaskManager();
+            taskManager.Add("a1");
+            taskManager.Add("b21");
+            taskManager.Add("b211");
+
+            Console.WriteLine(taskManager.Find("a1"));
+            Console.WriteLine(taskManager.Find("a2"));
+
+            taskManager.Remove("asd");
+            taskManager.Remove("b21");
+
+            taskManager.Display();
+
+
+            /*        Random r = new Random();
+                    int K = 0;
+
+                    for (int j = 0; j <10; j++) {
+                        K = r.Next(0, 2);
+                        if (K==0)
+                        {
+                            monsters.Add(new Boss());
+                        }
+                        else
+                        {
+                            monsters.Add(new Goblin());
+                        }
+                        monsters[j].Attack();
+                    }*/
+
+            /*     foreach (Monster monster in monsters)
+                 {
+                     if (monster is Goblin)
+                     {
+                        monster.Attack();
+                     }
+                 }*/
+
+        }
+    }
+}
---
+++ C#进阶学习/9.0顺序存储和链式存储/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System;
+
+namespace _9._0顺序存储和链式存储
+{
+    public class LinkedNode<T>
+    {
+        public T value;
+        public LinkedNode<T> nextNode;
+
+        public LinkedNode(T value)
+        {
+            this.value = value;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        public LinkedNode<T> head;
+        public LinkedNode<T> last;
+
+        //泛型还是要加强学习
+        public void Add(T value)
+        {
+            LinkedNode<T> node = new LinkedNode<T>(value);
+            if (head == null)
+            {
+                head = node;
+                last = node;
+            }
+            else
+            {
+                last.nextNode = node;
+                last = node;
+            }
+        }
+
+        public void Remove(T node)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("链表为空，无法删除。");
+                return;
+            }
+
+            if (head.value.Equals(node))
+            {
+                head = head.nextNode;
+                //
+                if (head==null)
+                {
+                    last = null;
+                }
+                return;
+            }
+      
+            LinkedNode<T> temp = last;
+            while (temp.nextNode != null)
+            {
+                if (temp.value.Equals(node))
+                {
+                    
+                    temp.nextNode = temp.nextNode.nextNode;
+                }
+                else
+                {
+                    temp = temp.nextNode;
+                }
+             
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            LinkedNode<int> t1 = new LinkedNode<int>(1);
+            LinkedNode<int> t2 = new LinkedNode<int>(2);
+            t1.nextNode = t2;
+            Console.WriteLine(t1.nextNode.value);
+            
+        }
+    }
+}
