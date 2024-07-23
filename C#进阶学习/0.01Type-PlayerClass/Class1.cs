6d3c730aa8c5d1d11a68895815960a05719320ea
VIKTOR RENEV <easykillme@outlook.com>
2024-07-23 21:01:51 +0900

    学习有点枯燥,准备学Unity项目

---
+++ C#进阶学习/0.01Type-PlayerClass/Class1.cs
@@ -0,0 +1,45 @@
+﻿namespace _0._01Type_PlayerClass
+{
+    public class Class1
+    {
+
+    }
+    public class Player
+    {
+        public string name;
+        public int health;
+        public int defence;
+        public int attack;
+        public int x,y;
+
+        public Player()
+        { }
+
+        public Player(int x,int y,string name)
+        {
+            this.x = x;
+            this.y = y;
+            this.name = name;
+        }
+
+        public Player(int x, int y, string name, int health, int defence, int attack) : this(x,y,name)
+        {
+            this.health = health;
+            this.defence = defence;
+            this.attack = attack;
+        }
+
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("X");
+        }
+        public void Clear()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("  ");
+        }
+    }
+}
---
+++ C#进阶学习/9.11关键类Assembly和Activator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+
+namespace _9._11关键类Assembly和Activator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly= Assembly.LoadFile(@"C:\Users\david\source\repos\davidcrybao\c-sharp-\C#进阶学习\0.01Type-PlayerClass\bin\Debug\net8.0\0.01Type-PlayerClass.dll");
+
+            //Assembly.LoadFrom(" 路径");
+            Type player = assembly.GetType("_0._01Type_PlayerClass.Player");
+            Type[] types= assembly.GetTypes();
+            foreach (var item in types)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("asdsaaaaaaaaaa");
+            MemberInfo[] memberInfos = player.GetMethods();
+            foreach (var item in memberInfos)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine("***********************");
+
+
+            ConstructorInfo[] constructorInfos = player.GetConstructors();
+            foreach (var item in constructorInfos)
+            {
+                Console.WriteLine(item);
+
+            }
+
+
+            Console.WriteLine("***********************");
+
+
+            FieldInfo[] fields = player.GetFields();
+            foreach (var item in fields)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            object playerObj = Activator.CreateInstance(player, 5, 10, "wo");
+            MethodInfo draw = player.GetMethod("Draw");    
+            MethodInfo clear = player.GetMethod("Clear");
+
+            draw.Invoke(playerObj, null); 
+            Console.ReadKey();
+            clear.Invoke(playerObj, null);
+
+        }
+    }
+}
