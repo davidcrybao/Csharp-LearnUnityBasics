da4e75bf5cb6043f62c7276962240910e1009f10
VIKTOR RENEV <easykillme@outlook.com>
2024-07-27 11:12:41 +0900

    思路一：在使用时生成随机，然后根据随机值取出对应的砖块类 实现方法 在需要生成砖块时生成一个随机值。 根据这个随机值创建对应的砖块类。 获取砖块类的随机存储信息。 ----------------------------------------------------- 思路二：创建一个字典，存储砖块类型和对应类型的砖块信息类 实现方法 在初始化时创建一个字典，存储砖块类型和对应的砖块信息类。 在需要生成砖块时，从字典中直接获取对应的砖块信息。
    #之前有个错误是Draw代码里面设计到了位置的更新

--- C#进阶实践练习/C#进阶实践练习/TetrisHandler.cs
+++ C#进阶实践练习/C#进阶实践练习/TetrisHandler.cs
@@ -6,89 +6,86 @@ using System.Threading.Tasks;
 
 namespace C_进阶实践练习
 {
-
-    internal class TetrisHandler 
+    internal class TetrisHandler
     {
-        E_CubeTypes type;
-        int index = 0;
-        int cubeType = 0;
-        Random r = new Random();
-        TetrisInfo tetrisInfo;
-        DrawObject[] drawObjects = new DrawObject[3];
-        DrawObject originObject;
+        private int randomPosition = 0;
+
+        private Dictionary<E_CubeTypes, TetrisInfo> tetrisInfo;
+
+        private DrawObject originObject;
+        private List<DrawObject> currentDraws;
+
+        private TetrisInfo currentTetrisInfo;
+
         public TetrisHandler()
         {
             Initialize();
         }
+
         private void GetRandomCube()
         {
-            index = r.Next(0, 101);
-            if (index <= 100 && index > 80)
-            {
-                type = E_CubeTypes.O_piece;
-            }
-            else if (index <= 80 && index > 60)
-            {
-                type = E_CubeTypes.I_piece;
-            }
-            else if (index <= 60 && index > 40)
-            {
-                type = index > 50 ? E_CubeTypes.S_piece : E_CubeTypes.Z_piece;
+            Random r = new Random();
+            //0是wall不考虑,得一个随机的类型
+            int typeIndex = r.Next(1, 8);
+            E_CubeTypes type = (E_CubeTypes)typeIndex;
 
-            }
-            else if (index <= 40 && index > 20)
+            currentDraws = new List<DrawObject>() {
+            new DrawObject(type),new DrawObject(type),new DrawObject(type)
+            };
+
+            for (int i = 0; i < currentDraws.Count; i++)
             {
-                type =index>30?  E_CubeTypes.J_piece : E_CubeTypes.L_piece;
+                currentDraws[i] = new DrawObject(type);
             }
-            else
+
+            //获得随机的位置
+            currentTetrisInfo = tetrisInfo[type];
+            randomPosition = r.Next(0, currentTetrisInfo.Count);
+            Position[] position = currentTetrisInfo[randomPosition];
+
+            //初始位置暂定测试用的
+            originObject = new DrawObject(5, 5, type);
+
+            for (int i = 0; i < currentDraws.Count; i++)
             {
-                type = E_CubeTypes.T_Piece;
+                currentDraws[i].position = position[i] + originObject.position;
+                //     new Position(position[i].x + originObject.position.x, position[i].y + originObject.position.y);
             }
         }
 
         public void Initialize()
         {
+            tetrisInfo = new Dictionary<E_CubeTypes, TetrisInfo>() {
+                {E_CubeTypes.O_piece ,new TetrisInfo (E_CubeTypes.O_piece)},
+                  {E_CubeTypes.T_Piece ,new TetrisInfo (E_CubeTypes.T_Piece)},
+                  {E_CubeTypes.I_piece ,new TetrisInfo (E_CubeTypes.I_piece)},
+                  {E_CubeTypes.S_piece ,new TetrisInfo (E_CubeTypes.S_piece)},
+                  {E_CubeTypes.Z_piece ,new TetrisInfo (E_CubeTypes.Z_piece)},
+                  {E_CubeTypes.J_piece ,new TetrisInfo (E_CubeTypes.J_piece)},
+                  {E_CubeTypes.L_piece ,new TetrisInfo (E_CubeTypes.L_piece)}
+            };
+
             GetRandomCube();
-            tetrisInfo = new TetrisInfo(type);
-
-            originObject = new DrawObject(5, 5, type);
-            //获得一个随机的队列来返回我们的类型
-            cubeType = r.Next(0, tetrisInfo.Count);
-
-            for (int i = 0; i < drawObjects.Length; i++)
-            {
-                drawObjects[i] = new DrawObject(type);
-            }
-
         }
 
-        public void SyncPosition()
+        public void Draw()
         {
-            Position[] position = tetrisInfo[index];
-            for (int i = 0; i < drawObjects.Length; i++)
-            {
-                drawObjects[i].position = new Position(position[i].x+originObject.position.x,position[i].y+originObject.position.y);
-            }
-        
-
-        }
-        public  void Draw()
-        {
-            SyncPosition();
             originObject.Draw();
-            for (int i = 0; i < drawObjects.Length; i++)
+            for (int i = 0; i < currentDraws.Count; i++)
             {
-                  drawObjects[i].Draw();
+                currentDraws[i].Draw();
             }
         }
+
         public void CWRotate()
         {
-            index++;
+            randomPosition++;
             Draw();
         }
+
         public void CCWRotate()
         {
-            index--;
+            randomPosition--;
             Draw();
         }
     }
